95e03f92553f63e68c06045c976bc292d77b969b
Petr Chromec <mortal_flesh@seznam.cz>
2019-08-29 09:58:46 +0200

    Introduce echo and string type

--- index.php
+++ index.php
@@ -4,17 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Party Hard</title>
+    <title>Party <?php echo "Hard"; ?></title>
 </head>
 <body>
-    <h1>Party Hard</h1>
+    <h1>Party <?php echo "Hard"; ?></h1>
 
     <ul>
-        <li>Pořadatel: Petr</li>
-        <li>Čas: 31.8. 2019</li>
-        <li>Místo: Praha</li>
-        <li>Počet míst: 30</li>
-        <li>Zbývá míst: 30</li>
+        <li>Pořadatel: <?php echo "Petr"; ?></li>
+        <li>Čas: 31. 8. 2019</li>
+        <li>Místo: <?php echo "Praha"; ?></li>
+        <li>Počet míst: <?php echo 30; ?></li>
+        <li>Zbývá míst: <?php echo 30 - 5; ?></li>
+        <li>Příjde: <?php echo 5; ?></li>
     </ul>
 </body>
 </html>
