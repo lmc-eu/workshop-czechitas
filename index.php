6a290fc2c68837c68d913087e7d0d8fb14a83153
Petr Chromec <mortal_flesh@seznam.cz>
2019-08-30 09:55:37 +0200

    Introduce variables

--- index.php
+++ index.php
@@ -1,21 +1,31 @@
+<?php
+
+$nazevParty = "Hard";
+$poradatel = "Petr";
+$misto = "Praha";
+$kapacita = 30;
+$pocetUcastniku = 5;
+$volnaKapacita = $kapacita - $pocetUcastniku;
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Party <?php echo "Hard"; ?></title>
+    <title>Party <?php echo $nazevParty; ?></title>
 </head>
 <body>
-    <h1>Party <?php echo "Hard"; ?></h1>
+    <h1>Party <?php echo $nazevParty; ?></h1>
 
     <ul>
-        <li>Pořadatel: <?php echo "Petr"; ?></li>
+        <li>Pořadatel: <?php echo $poradatel; ?></li>
         <li>Čas: 31. 8. 2019</li>
-        <li>Místo: <?php echo "Praha"; ?></li>
-        <li>Počet míst: <?php echo 30; ?></li>
-        <li>Zbývá míst: <?php echo 30 - 5; ?></li>
-        <li>Příjde: <?php echo 5; ?></li>
+        <li>Místo: <?php echo $misto; ?></li>
+        <li>Počet míst: <?php echo $kapacita; ?></li>
+        <li>Zbývá míst: <?php echo $volnaKapacita; ?></li>
+        <li>Příjde: <?php echo $pocetUcastniku; ?></li>
     </ul>
 </body>
 </html>
