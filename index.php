d0f0d9a0f35d723caa640b0d7bab0e648a9ddb48
Petr Chromec <petr.chromec@lmc.eu>
2019-08-30 13:52:08 +0200

    Introduce echo

--- index.php
+++ index.php
@@ -4,16 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Party Czechitas</title>
+    <title>Party <?php echo "Czechitas"; ?></title>
 </head>
 <body>
-    <h1>Party Czechitas</h1>
+    <h1>Party <?php echo "Czechitas"; ?></h1>
 
     <ul>
-        <li>Pořadatel: Jirka, Petr, Ondra</li>
-        <li>Místo: Praha</li>
-        <li>Počet míst: 30</li>
-        <li>Zbývá míst: 30</li>
+        <li>Pořadatel: <?php echo "Petr, Jirka, Ondra"; ?></li>
+        <li>Místo: <?php echo "Praha"; ?></li>
+        <li>Počet míst: <?php echo 30; ?></li>
+        <li>Zbývá míst: <?php echo 30; ?></li>
     </ul>
 </body>
 </html>
