a434c219727dd1da05da390eb8fcf3cad6e20610
Petr Chromec <petr.chromec@lmc.eu>
2019-08-30 13:48:37 +0200

    Add content

--- index.php
+++ index.php
@@ -4,9 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Party Czechitas</title>
 </head>
 <body>
-    
+    <h1>Party Czechitas</h1>
+
+    <ul>
+        <li>Pořadatel: Jirka, Petr, Ondra</li>
+        <li>Místo: Praha</li>
+        <li>Počet míst: 30</li>
+        <li>Zbývá míst: 30</li>
+    </ul>
 </body>
 </html>
