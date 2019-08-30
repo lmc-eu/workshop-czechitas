d958a185352ef030435d9a8c8e5d7ec6c51a0570
Petr Chromec <petr.chromec@lmc.eu>
2019-08-30 19:53:11 +0200

    Introduce variables

--- index.php
+++ index.php
@@ -1,20 +1,31 @@
+<?php
+
+$nazevAkce = "Párty Czechitas";
+$poradatel = "LMC";
+$mistoKonani = "Praha";
+
+$kapacita = 30;
+$prihlaseno = 5;
+$zbyvaMist = $kapacita - $prihlaseno;
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?php echo "Párty Czechitas"; ?></title>
+    <title><?php echo $nazevAkce; ?></title>
 </head>
 <body>
-    <h1><?php echo "Párty Czechitas"; ?></h1>
+    <h1><?php echo $nazevAkce; ?></h1>
 
     <ul>
-        <li>Pořadatel: <?php echo "LMC"; ?></li>
-        <li>Místo konání: <?php echo "Praha"; ?></li>
-        <li>Kapacita: <?php echo 30; ?></li>
-        <li>Přihlášeno účastníků: <?php echo 10; ?></li>
-        <li>Zbývá míst: <?php echo 20; ?></li>
+        <li>Pořadatel: <?php echo $poradatel; ?></li>
+        <li>Místo konání: <?php echo $mistoKonani; ?></li>
+        <li>Kapacita: <?php echo $kapacita; ?></li>
+        <li>Přihlášeno účastníků: <?php echo $prihlaseno; ?></li>
+        <li>Zbývá míst: <?php echo $zbyvaMist; ?></li>
     </ul>
 </body>
 </html>
