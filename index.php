cb4cda657a24c7efa3fda42c0e0d115090ccb6a7
Petr Chromec <petr.chromec@lmc.eu>
2019-08-30 19:46:12 +0200

    Add basic content

--- index.php
+++ index.php
@@ -4,9 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Párty Czechitas</title>
 </head>
 <body>
-    
+    <h1>Párty Czechitas</h1>
+
+    <ul>
+        <li>Pořadatel: LMC</li>
+        <li>Místo konání: Praha</li>
+        <li>Kapacita: 30</li>
+        <li>Přihlášeno účastníků: 10</li>
+        <li>Zbývá míst: 20</li>
+    </ul>
 </body>
 </html>
